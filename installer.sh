a3a00b36422a88179249961e8d129addafdb3e15
Ely Torres Neto <93855901+netoe1@users.noreply.github.com>
2024-04-17 23:56:25 -0300

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,8 @@
+echo "Welcome to the Installer!"
+git clone https://github.com/nvm-sh/nvm nvm-installer
+cd nvm-installer
+
+echo "Configuring permissions..."
+sudo chmod +x install.sh
+echo "Running install.sh script...."
+./install.sh
